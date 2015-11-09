427e229136909c2921719cc8ddfeb4d18ce64f2b
Flinn Gendall <email": null>
2015-11-09 16:11:57 +1300

    add moveCharacter function to dungeon

--- server/dungeon/dungeon.js
+++ server/dungeon/dungeon.js
@@ -1,15 +1,30 @@
 var Dungeon = (function() {
   function Dungeon(size){
+    this.charLoc = {}
     this.size = size
-    this.maze = emptyMaze(size)
+    this.maze = buildEmptyMaze(size)
     placeCharacter(this)
+    placeTreasure(this)
   }
 
-  Dungeon.prototype.placeCharacter = function(){
-    this.maze[randomCoord()][randomCoord()] = 'c'
+  Dungeon.prototype.moveCharacter = function(dir){
+    var directions = {up: {x: -1, y: 0},
+                      down: {x: 1, y: 0},
+                      left: {x: 0, y: -1},
+                      right: {x: 0, y: 1}}
+    this.charLoc = merge(this.charLoc,directions[dir])
   }
 
-  function emptyMaze(size){
+  Dungeon.prototype.setCharLoc = function(x,y){
+    this.charLoc = {x: x, y: y}
+    removeCharRef(this)
+    setCharRef(this)
+  }
+
+  //=========PRIVATE FUNCTIONS============
+
+
+  function buildEmptyMaze(size){
     var maze = new Array(size)
     for (var i=0; i<size; i++){
       maze[i] = new Array(size)
@@ -21,11 +36,41 @@ var Dungeon = (function() {
   }
 
   function placeCharacter(dung){
-    dung.maze[randomCoord(dung)][randomCoord(dung)] = 'c'
+    dung.charLoc = {x: randomCoord(dung), y: randomCoord(dung)}
+    setCharRef(dung)
+  }
+
+  function placeTreasure(dung){
+    dung.maze[randomCoord(dung)][randomCoord(dung)] = 't'
+  }
+
+  function removeCharRef(dung){
+    for(var i=0;i<dung.maze.length;i++){
+      for(var j=0;j<dung.maze.length;j++){
+        if(dung.maze[i][j] == 'c'){
+          dung.maze[i][j] == 'e'
+          foundChar = true
+          break
+        }
+      }
+      if(foundChar = true) break
+    }
+  }
+
+  function setCharRef(dung){
+    dung.maze[dung.charLoc.x][dung.charLoc.y] = 'c'
   }
 
   function randomCoord(dung){
     return Math.floor(Math.random()*(dung.size))
+  }
+
+  function merge(obj1,obj2){
+    var result = {}
+    for(i in obj1){
+      result[i] = obj1[i]+obj2[i]
+    }
+    return result
   }
 
   return Dungeon
--- server/test/dungeon_test.js
+++ server/test/dungeon_test.js
@@ -4,26 +4,47 @@ var expect = chai.expect
 
 describe('Dungeon', function(){
   before(function(){
-    this.testDung = new Dungeon(5)
+    this.testSize = 10
+    this.testDung = new Dungeon(this.testSize)
   })
 
   it('Has a maze of correct size', function(){
-    expect(this.testDung.maze.length).to.equal(5)
-    expect(this.testDung.maze[0].length).to.equal(5)
+    expect(this.testDung.maze.length).to.equal(this.testSize)
+    expect(this.testDung.maze[0].length).to.equal(this.testSize)
   })
 
   it('Has a character at a starting position', function(){
-      var foundChar = false
-      for(var i=0;i<this.testDung.maze.length;i++){
-        for(var j=0;j<this.testDung.maze.length;j++){
-          if(this.testDung.maze[i][j] == "c"){
-            foundChar = true
-            break
-          }
+    var foundChar = false
+    for(var i=0;i<this.testDung.maze.length;i++){
+      for(var j=0;j<this.testDung.maze.length;j++){
+        if(this.testDung.maze[i][j] == "c"){
+          foundChar = true
+          break
         }
-        if(foundChar = true) break
       }
+      if(foundChar = true) break
+    }
     expect(foundChar).to.be.true
+  })
+
+  it('Has treasure hidden somewhere', function(){
+    var foundChar = false
+    for(var i=0;i<this.testDung.maze.length;i++){
+      for(var j=0;j<this.testDung.maze.length;j++){
+        if(this.testDung.maze[i][j] == "t"){
+          foundChar = true
+          break
+        }
+      }
+      if(foundChar = true) break
+    }
+    expect(foundChar).to.be.true
+  })
+
+  it('Can move character around maze', function(){
+    this.testDung.setCharLoc(0,0)
+    this.testDung.moveCharacter('down')
+    expect(this.testDung.charLoc.x).to.equal(1)
   })
 
 })
